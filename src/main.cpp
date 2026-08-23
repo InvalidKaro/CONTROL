633f1f9a115b4a41995ad8c4fec158c5fee09d8f
Karo <76846752+InvalidKaro@users.noreply.github.com>
2026-08-23 02:13:28 +0200

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,22 @@
 #include <Arduino.h>
+
 #include "ControlOS.h"
+#include "RuntimeDebug.h"
 
 ControlOS os;
 
 void setup() {
+  RuntimeDebug::begin();
+
+  RuntimeDebug::mark(
+      RuntimeDebug::Stage::EnterSetup,
+      "Arduino setup() entered"
+  );
+
   os.begin();
 }
 
 void loop() {
+  RuntimeDebug::heartbeat();
   os.loop();
 }
